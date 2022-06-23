d18c7c285f450007c2f943f13f3b1b57d9f3b069
sokkanen <joel.sokkanen@helsinki.fi>
2022-06-23 10:48:11 +0300

    More work on other branch.

--- apps/project_front/src/App.js
+++ apps/project_front/src/App.js
@@ -17,7 +17,6 @@ const TodoForm = ({ addTodo }) => {
       <form onSubmit={addTodo}>
         <input id="new_todo" type="text" placeholder="Type in your todo"/>
         <button type="submit" >Add ToDo</button>
-        <h1>Wow, Greetings from another branch!</h1>
       </form>
     </div>
   )
